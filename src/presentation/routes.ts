091408d301d7393608d5ff60d5aa6b3024acf1c5
JonDacosta <dacostaafonso@gmail.com>
2024-04-25 12:33:33 +0200

    update - controller 'separar rutas post'

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -2,7 +2,11 @@ import { Router } from "express";
 import { AuthRoutes } from "./auth/routes";
 
 export class AppRoutes {
+
+    
     static get routes(): Router {
+
+
         const router = Router();
 
         //Rutas principales
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -14,16 +14,16 @@ export class Server {
     private readonly routes: Router;
 
     constructor( options:Options ) {
-        const { port = 3100, routes }  = options
+        const { port = 3000, routes }  = options
 
         this.port = port;
         this.routes = routes;
     }
 
     async start() {
-
+        //use port
         this.app.use( this.routes );
-        
+        //listening
         this.app.listen(this.port, () => {
             console.log(`Server running on port ${ this.port }`);
         })
