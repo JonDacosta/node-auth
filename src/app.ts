db5423776bd9eba41522aea008b3f78c9a3626f8
JonDacosta <dacostaafonso@gmail.com>
2024-04-25 12:08:14 +0200

    update files

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,17 @@
+import { envs } from "./config";
 import { Server } from "./presentation/server";
 
 
 (() => {
    main(); 
-})
+})()
 
 async function main() {
     // await base de datos
 
     //to-to inicio de server
     new Server({
-        
+        port: envs.PORT
     })
         .start
 }
