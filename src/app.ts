d5cc2d6bd37d015276d392159647941b039807a9
JonDacosta <dacostaafonso@gmail.com>
2024-04-29 19:22:30 +0200

    New - envs del archivo env para await de la DB

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { envs } from "./config";
 import { AppRoutes } from "./presentation/routes";
 import { Server } from "./presentation/server";
+import { MongoDatabase } from './data/mongodb/mongo-database';
 
 
 (() => {
@@ -8,7 +9,10 @@ import { Server } from "./presentation/server";
 })()
 
 async function main() {
-    // await base de datos
+    await MongoDatabase.connect({
+        dbName: envs.MONGO_DB_NAME,
+        mongoUrl: envs.MONGO_URL
+    })
 
     //to-to inicio de server
     new Server({
