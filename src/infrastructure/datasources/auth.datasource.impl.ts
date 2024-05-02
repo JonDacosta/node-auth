b87312784ec420f4ffdc9efdb8b0f2d1a8845414
JonDacosta <dacostaafonso@gmail.com>
2024-05-02 13:35:29 +0200

    Add - Inyección de dependencias Bcrypt

--- src/infrastructure/datasources/auth.datasource.impl.ts
+++ src/infrastructure/datasources/auth.datasource.impl.ts
@@ -2,7 +2,17 @@ import { BcryptAdapter } from "../../config/bcrypt";
 import { UserModel } from "../../data/mongodb";
 import { AuthDatasource, CustomError, RegisterUserDto, UserEntity } from "../../domain";
 
+type HashFunction = (password: string) => string;
+type CompareFunction = (password: string, hashed: string) => boolean;
+
 export class AuthDatasourceImpl implements AuthDatasource {
+
+    constructor(
+        private readonly hashPassword: HashFunction = BcryptAdapter.hash,
+        private readonly compareFunction: CompareFunction = BcryptAdapter.compare,
+    ){
+
+    }
 
     async register( registerUserDto: RegisterUserDto ): Promise<UserEntity> {
         
@@ -17,7 +27,7 @@ export class AuthDatasourceImpl implements AuthDatasource {
             const user = await UserModel.create({
                 name: name,
                 email: email,
-                password: BcryptAdapter.hash(password),
+                password: this.hashPassword(password),
             });
 
             //Hash del pass
