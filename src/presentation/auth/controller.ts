ea7f89926eae898fdc49eeb2a1de8cdce136c255
JonDacosta <dacostaafonso@gmail.com>
2024-05-06 12:08:22 +0200

    ADD - JWT

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import { JwtAdapter } from '../../config';
 import { AuthRepository, CustomError, RegisterUserDto } from '../../domain';
 
 
@@ -18,14 +19,17 @@ export class AuthController {
         return res.status(500).json
     }
 
-    registerUser = ( req: Request, res: Response ) => {
+    registerUser = async ( req: Request, res: Response ) => {
 
         const [error, registerUserDto] = RegisterUserDto.create(req.body);
         if( error ) return res.status(400).json({ error });
 
 
         this.authRepository.register(registerUserDto!)
-            .then( user => res.json(user))
+            .then( async (user) => res.json({
+                user,
+                token: await JwtAdapter.generateToken({ email: user.email })
+            }))
             .catch( error => this.handleError(error, res) )
     }
 
