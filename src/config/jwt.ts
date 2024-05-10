5e801622fe24ae8288b07a9c8ccf70f3fd0b2ead
JonDacosta <dacostaafonso@gmail.com>
2024-05-10 12:39:37 +0200

    Change - jwt

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -1,13 +1,18 @@
 import jwt from 'jsonwebtoken';
+import { envs } from './envs';
+
+const JWT_SEED = envs.JWT_SEED;
+
 
 export class JwtAdapter {
+    
 
     static async generateToken( payload: Object, duration: string = '2h' ):Promise<string|null> {
         
        return new Promise( ( resolve ) => {
             
-            //TO DO generar el seed
-            jwt.sign( payload, 'SEED', { expiresIn: duration }, ( err, token) => {
+            
+            jwt.sign( payload, JWT_SEED, { expiresIn: duration }, ( err, token) => {
 
                 if( err ) return resolve(null);
 
@@ -18,7 +23,20 @@ export class JwtAdapter {
 
 
     }
+    
+    static validateToken<T>( token: string ): Promise<T | null> {
 
+        return new Promise( (resolve) => {
+
+            jwt.verify( token, JWT_SEED, (err, decoded) => {
+                
+                if( err ) return resolve(null);
+
+                resolve(decoded as T);
+
+            } )
+        })
+    }
 
 
 }
