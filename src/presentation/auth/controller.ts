c2d45d71f23534fc39202f8bc11f5ba9a56a561a
JonDacosta <dacostaafonso@gmail.com>
2024-05-10 12:39:05 +0200

    Add - Middleware

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { JwtAdapter } from '../../config';
 import { UserModel } from '../../data/mongodb';
-import { AuthRepository, CustomError, RegisterUserDto } from '../../domain';
+import { AuthRepository, CustomError, RegisterUser, RegisterUserDto } from '../../domain';
 
 
 export class AuthController {
@@ -20,18 +20,16 @@ export class AuthController {
         return res.status(500).json
     }
 
-    registerUser = async ( req: Request, res: Response ) => {
+    registerUser = ( req: Request, res: Response ) => {
 
         const [error, registerUserDto] = RegisterUserDto.create(req.body);
         if( error ) return res.status(400).json({ error });
 
 
-        this.authRepository.register(registerUserDto!)
-            .then( async (user) => res.json({
-                user,
-                token: await JwtAdapter.generateToken({ id: user.id })
-            }))
-            .catch( error => this.handleError(error, res) )
+        new RegisterUser(this.authRepository)
+            .execute( registerUserDto! )
+            .then( data => res.json(data) )
+            .catch( error => this.handleError(error, res));
     }
 
     loginUser = ( req: Request, res: Response ) => {
@@ -41,8 +39,13 @@ export class AuthController {
     getUsers = ( req: Request, res: Response ) => {
         
         UserModel.find()
-        .then( users => res.json(users) )
+        .then( users => {
+            res.json({
+                //users,
+                user: req.body.user
+            })
+        })
         .catch(() => res.status(500).json({ error: 'Internal server error' }))
-        
+
     }
 }
